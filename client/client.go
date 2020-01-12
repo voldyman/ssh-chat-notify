400e8182adbc9b982a9cdb7ed806f3fd6c6bfc7f
Akshay Shekher <akshay.shkekher@gmail.com>
2020-01-11 21:12:21 -0800

    Updated client to handle connection close and ratelimiting

--- client/client.go
+++ client/client.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"time"
 
 	"github.com/lunixbochs/vtclean"
+	"github.com/shazow/rateio"
 	"golang.org/x/crypto/ssh"
 )
 
@@ -18,6 +20,10 @@ type Client struct {
 
 	scanner *bufio.Scanner
 	writer  io.Writer
+
+	err error
+
+	ratelimit rateio.Limiter
 }
 
 // CreateClient establishes a connections with the destination as the given username
@@ -30,39 +36,53 @@ func CreateClient(destination, username string) (*Client, error) {
 	if err != nil {
 		return nil, fmt.Errorf("unable to open read/write connection to the session: %w", err)
 	}
+
 	return &Client{
 		sshSession: sshSession,
 		scanner:    bufio.NewScanner(r),
 		writer:     w,
+		ratelimit:  rateio.NewSimpleLimiter(3, time.Second*3),
 	}, nil
 }
 
 // ScanLine reads the connection till the next new line
-func (s *Client) ScanLine() (string, error) {
-	continueReading := s.scanner.Scan()
-	if !continueReading {
-		return "", s.scanner.Err()
+func (c *Client) ScanLine() (string, error) {
+	if c.err != nil {
+		return "", c.err
 	}
-	cleanedLine := vtclean.Clean(s.scanner.Text(), noColor)
+	continueReading := c.scanner.Scan()
+	if !continueReading {
+		c.err = io.EOF
+		return "", c.err
+	}
+
+	if c.scanner.Err() != nil {
+		c.err = c.scanner.Err()
+		return "", c.err
+	}
+	cleanedLine := vtclean.Clean(c.scanner.Text(), noColor)
 	return cleanedLine, nil
 }
 
 // WriteLine send the given line to ssh-chat
-func (s *Client) WriteLine(line string) error {
-	_, err := s.writer.Write([]byte(line + "\r\n"))
+func (c *Client) WriteLine(line string) error {
+	if c.ratelimit.Count(1) != nil {
+		time.Sleep(1 * time.Second)
+	}
+	return c.writeLine(line)
+}
+
+func (c *Client) writeLine(line string) error {
+	_, err := c.writer.Write([]byte(line + "\r\n"))
 	if err != nil {
 		return fmt.Errorf("unable to write line: %w", err)
-	}
-	// discard the echo
-	if discardEcho {
-		s.ScanLine()
 	}
 	return nil
 }
 
 // Close disconnects the client
-func (s *Client) Close() error {
-	err := s.sshSession.Close()
+func (c *Client) Close() error {
+	err := c.sshSession.Close()
 	if err != nil {
 		return fmt.Errorf("unable to close underlying ssh session: %w", err)
 	}
