11e1a81aa29615d3778614f7cf3f9c3fa8500969
Akshay Shekher <akshay.shkekher@gmail.com>
2020-01-11 21:13:10 -0800

    updated main to start using bot for business logic

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/alexcesaro/log"
 	"github.com/alexcesaro/log/golog"
 	"github.com/voldyman/ssh-chat-notify/client"
+	"github.com/voldyman/ssh-chat-notify/notifyi"
 	"github.com/voldyman/ssh-chat-notify/parser"
 )
 
@@ -22,6 +24,21 @@ func main() {
 		logger.Error("Failed:", err)
 	}
 }
+
+type clientComms struct {
+	client *client.Client
+}
+
+func (c *clientComms) PrivateMessage(toUsername, message string) error {
+	c.client.WriteLine(fmt.Sprintf("/msg %s %s", toUsername, message))
+	return nil
+}
+
+func (c *clientComms) PublicMessage(message string) error {
+	c.client.WriteLine(message)
+	return nil
+}
+
 func run() error {
 	dest := sshChatHost
 	if len(os.Args) >= 2 {
@@ -34,6 +51,7 @@ func run() error {
 	}
 	defer client.Close()
 
+	bot := notifyi.New(username, &clientComms{client})
 	lineParser := parser.New()
 
 	for {
@@ -42,6 +60,7 @@ func run() error {
 			return err
 		}
 
+		fmt.Println("Got Line", line)
 		parsedResult, err := lineParser.Parse([]byte(line))
 		if err != nil {
 			logger.Warningf("parsing failed: %+v", err)
@@ -49,21 +68,31 @@ func run() error {
 		}
 
 		switch result := parsedResult.(type) {
-		case parser.UsernameChangeMsg:
-			logger.Info("Nick change", quote(result.FromUsername), "->", result.ToUsername)
-
-		case parser.JoinMsg:
-			logger.Info("User", quote(result.Username), "has", result.Status)
-
 		case parser.PrivateMsg:
 			logger.Info("Private Message", quote(result.From), "->", result.Message)
-			client.WriteLine("stop tickeling me " + result.From)
+			bot.PrivateMessage(result.From, result.Message)
 
 		case parser.PublicMsg:
 			logger.Info("Public Message", quote(result.From), "->", result.Message)
+			bot.PublicMessage(result.From, result.Message)
 
 		case parser.ActionMsg:
 			logger.Info("Smartass says:", quote(result.From), "->", result.Message)
+			bot.ActionMessage(result.From, result.Message)
+
+		case parser.UsernameChangeMsg:
+			logger.Info("Nick change", quote(result.FromUsername), "->", result.ToUsername)
+			bot.UsernameChangeMessage(result.FromUsername, result.ToUsername)
+
+		case parser.JoinMsg:
+			switch result.Status {
+			case parser.UserJoined:
+				bot.UserJoinedMessage(result.Username)
+			case parser.UserLeft:
+				bot.UserLeftMessage(result.Username)
+			}
+			logger.Info("User", quote(result.Username), "has", result.Status)
+
 		}
 
 	}
