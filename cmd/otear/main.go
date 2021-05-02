1ec87da21f775768ef8a9175f2ec2b4aa98c9334
Akshay <akshay.shekher@gmail.com>
2021-05-02 13:32:17 -0700

    Added otea, simple ssh-chat scanner/notifier

---
+++ cmd/otear/main.go
@@ -0,0 +1,150 @@
+// otear -> scan in Spanish
+// scans the stream of messages from ssh-chat
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	flags "github.com/jessevdk/go-flags"
+	"github.com/pkg/errors"
+	lg "github.com/sirupsen/logrus"
+	sshclient "github.com/voldyman/ssh-chat-notify/client"
+)
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+type Options struct {
+	ServerAddr       string   `short:"s" long:"server-addr" description:"ssh-chat server address to connect to" required:"true"`
+	BotName          string   `short:"n" long:"name" description:"name of the bot listener bot" required:"true"`
+	Keywords         []string `short:"k" long:"keywords" description:"keywords to monitor" required:"true"`
+	PushoverToken    string   `short:"p" long:"pushover-token" description:"token used to send notifications to pushover" required:"true"`
+	PushoverGroupKey string   `short:"g" long:"pushover-group" description:"pushover group to notify" required:"true"`
+
+	Verbose         bool   `short:"v" long:"verbose" description:"print verbose messages"`
+	LogTimeLocation string `short:"z" long:"log-tz" description:"timezone for log messages" default:"America/Vancouver"`
+}
+
+func run() error {
+	var opts Options
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		return nil
+	}
+	err = setupLogger(opts)
+	if err != nil {
+		return err
+	}
+
+	client, err := sshclient.CreateClient(opts.ServerAddr, opts.BotName)
+	if err != nil {
+		return errors.Wrapf(err, "connect failed")
+	}
+	defer client.Close()
+
+	for {
+		cline, err := client.ScanLine()
+		if err != nil {
+			return errors.Wrapf(err, "read failed")
+		}
+		line := strings.TrimSpace(cline)
+
+		lg.WithField("line", line).Debug("Scanned line")
+
+		if strings.HasPrefix(line, "*") {
+			lg.Debug("Ignoring system message")
+			continue
+		}
+
+		parts := strings.SplitN(line, ":", 2)
+		if len(parts) != 2 {
+			lg.Warn("Message does not have a sender, ignoring")
+		}
+		from := strings.TrimSpace(parts[0])
+		msg := strings.TrimSpace(parts[1])
+
+		found := checkKeyword(msg, opts.Keywords)
+
+		if !found {
+			continue
+		}
+		lg.WithFields(lg.Fields{"from": from, "message": msg}).
+			Info("Notifying for message from")
+		sendPushoverNotification(opts, from, msg)
+	}
+}
+
+func checkKeyword(str string, keywords []string) bool {
+	for _, word := range keywords {
+		if strings.Contains(str, word) {
+			lg.WithField("word", word).
+				Info("Found keyword")
+
+			return true
+		}
+	}
+	return false
+}
+
+const pushoverMessageURL = "https://api.pushover.net/1/messages.json"
+
+func sendPushoverNotification(opts Options, from, message string) {
+	params := url.Values{}
+	params.Add("token", opts.PushoverToken)
+	params.Add("user", opts.PushoverGroupKey)
+	params.Add("title", "SSH Chat Mention")
+	params.Add("message", fmt.Sprintf("%s said: %s", from, message))
+
+	resp, err := http.PostForm(pushoverMessageURL, params)
+	if err != nil {
+		lg.Warn("Unable to publish to pushover", err)
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		lg.WithField("status", resp.Request.Response.Status).
+			Warn("Unexpected status code from pushover", resp)
+		return
+	}
+}
+func setupLogger(opts Options) error {
+	if opts.Verbose {
+		lg.SetLevel(lg.DebugLevel)
+	} else {
+		lg.SetLevel(lg.InfoLevel)
+	}
+	timeZone, err := time.LoadLocation(opts.LogTimeLocation)
+	if err != nil {
+		return errors.Wrapf(err, "unable to setup logger for location %s", opts.LogTimeLocation)
+	}
+
+	lg.SetFormatter(newTzFormatter(timeZone))
+
+	return nil
+}
+
+type TZLogFormatter struct {
+	location *time.Location
+	fmtr     *lg.TextFormatter
+}
+
+func newTzFormatter(loc *time.Location) lg.Formatter {
+	return &TZLogFormatter{
+		location: loc,
+		fmtr: &lg.TextFormatter{
+			FullTimestamp:   true,
+			TimestampFormat: time.RFC822,
+		},
+	}
+}
+
+func (tz *TZLogFormatter) Format(e *lg.Entry) ([]byte, error) {
+	e.Time = e.Time.In(tz.location)
+	return tz.fmtr.Format(e)
+}
--- go.mod
+++ go.mod
@@ -4,8 +4,14 @@ go 1.13
 
 require (
 	github.com/alexcesaro/log v0.0.0-20150915221235-61e686294e58
+	github.com/jessevdk/go-flags v1.5.0
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/lunixbochs/vtclean v1.0.0
+	github.com/pkg/errors v0.9.1
 	github.com/prataprc/goparsec v0.0.0-20180806094145-2600a2a4a410
 	github.com/shazow/rateio v0.0.0-20200113175441-4461efc8bdc4
+	github.com/sirupsen/logrus v1.8.1
+	github.com/stretchr/testify v1.4.0 // indirect
 	golang.org/x/crypto v0.0.0-20200109152110-61a87790db17
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
